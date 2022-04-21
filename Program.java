d840a9303f06d3421f53fb98267b6ec558f59ef4
Calder Johnson <calderbjohnson@icloud.com>
2022-04-21 19:19:27 -0400

    Refactored FEN to board and added the inverse

--- Program.java
+++ Program.java
@@ -1,6 +1,8 @@
 public class Program {
     public static void main(String[] args) {
         Board board = new Board();
+        board.FENtoPosition("8/5Q1p/3Nk3/p3P2p/8/7P/1PP2K2/4R3 b - - 1 35");
         board.printBoard();
+        System.out.println(board.boardToFEN());
     }
 }
